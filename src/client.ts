9d3a910a4040a83663d74fa0fc78c5e1e5345d4d
Ryusei Igarashi <160300516+11gather11@users.noreply.github.com>
2025-11-14 16:40:16 +0900

    Test (#2)
    * Fix server name in the root route response message
    
    * クライアントの型エクスポートとユーティリティ関数のコメントを追加
    
    * 新しい変更セットファイルを追加し、@hexcuit/serverのパッチを定義

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import { hc } from 'hono/client'
 import type { AppType } from './index'
 
+// クライアントの型をエクスポート
 export type Client = ReturnType<typeof hc<AppType>>
 
+// 型付きクライアントを作成するユーティリティ関数
 export const hcWithType = (...args: Parameters<typeof hc>): Client => hc<AppType>(...args)

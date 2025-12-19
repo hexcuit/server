3fd5952066c0414de25e183a7f5a2276692b2dff
11gather11 <11gather11@gmail.com>
2025-12-19 16:01:34 +0900

    refactor: move API key and CORS middleware to v1 routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { Scalar } from '@scalar/hono-api-reference'
 import { HTTPException } from 'hono/http-exception'
-import { apiKeyMiddleware } from '@/middlewares/apiKeyMiddleware'
-import { corsMiddleware } from '@/middlewares/corsMiddleware'
 import v1 from '@/routes/v1'
 import version from '../package.json'
 
 const app = new OpenAPIHono<{ Bindings: Cloudflare.Env }>()
+
+app.openAPIRegistry.registerComponent('securitySchemes', 'apiKey', {
+	type: 'apiKey',
+	in: 'header',
+	name: 'x-api-key',
+	description: 'API Key for authentication',
+})
 
 app.doc('/docs.json', {
 	openapi: '3.1.0',
@@ -17,17 +22,8 @@ app.doc('/docs.json', {
 	security: [{ apiKey: [] }],
 })
 
-app.openAPIRegistry.registerComponent('securitySchemes', 'apiKey', {
-	type: 'apiKey',
-	in: 'header',
-	name: 'x-api-key',
-	description: 'API Key for authentication',
-})
-
 app.get('/docs', Scalar({ url: '/docs.json' }))
 
-app.use(corsMiddleware)
-app.use(apiKeyMiddleware)
 app.route('/', v1)
 
 app.onError((err, c) => {
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,14 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { apiKeyMiddleware } from '@/middlewares/apiKeyMiddleware'
+import { corsMiddleware } from '@/middlewares/corsMiddleware'
 import guilds from './guilds'
 import queues from './queues'
 import ranks from './ranks'
 
 const app = new OpenAPIHono<{ Bindings: Cloudflare.Env }>()
 
+app.use(corsMiddleware)
+app.use(apiKeyMiddleware)
 app.route('/', ranks)
 app.route('/', queues)
 app.route('/', guilds)

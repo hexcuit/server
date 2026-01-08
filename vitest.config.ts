1e6d7ca14c6d978bdc7ec5b5aed6abf5d758958d
11gather11 <11gather11@gmail.com>
2026-01-08 16:48:16 +0900

    refactor: Move test-related files to mocks directory and update references

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@/': new URL('./src/', import.meta.url).pathname,
-			'@test/': new URL('./test/', import.meta.url).pathname,
+			'@test/': new URL('./mocks/', import.meta.url).pathname,
 		},
 	},
 	test: {
